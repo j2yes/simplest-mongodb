53a40411fad47e5ef140c50c6d47297dae95b2f8
j2yes <j2yess@gmail.com>
2018-05-24 00:34:04 +0900

    add filter function

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
 const MongoClient = require('mongodb').MongoClient;
+const COMPARISON_OPERATORS = {
+  equal: "$eq",
+  greaterThan: "$gt",
+  greaterThanOrEqual: "$gte",
+  include: "$in",
+  lessThan: "$lt",
+  lessThanOrEqual: "$lte",
+  notEqual: "$ne",
+  exclude: "$nin"
+};
 
 function MongoDB(MONGODB_PATH, DB_NAME) {
-    this.MONGODB_PATH = MONGODB_PATH;
-    this.DB_NAME = DB_NAME;
-    this.client = null;
+  this.MONGODB_PATH = MONGODB_PATH;
+  this.DB_NAME = DB_NAME;
+  this.COMPARISON_OPERATORS = COMPARISON_OPERATORS;
+  this.client = null;
 }
 
-MongoDB.prototype.getClient = function() {
-    let that = this;
-    if(that.client) {
-        return that.client;
-    } else {
-        return new Promise((resolve, reject) => {
-            MongoClient.connect(that.MONGODB_PATH, function (err, client) {
-                if (err) {
-                    console.log('err getClient', err);
-                    reject(err);
-                } else {
-                    console.log('success getClient', client.s.url);
-                    that.client = client;
-                    resolve(client);
-                }
-            });
-        });
-    }
+MongoDB.prototype.getClient = function () {
+  let that = this;
+  if (that.client) {
+    return that.client;
+  } else {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(that.MONGODB_PATH, function (err, client) {
+        if (err) {
+          console.log('err getClient', err);
+          reject(err);
+        } else {
+          console.log('success getClient', client.s.url);
+          that.client = client;
+          resolve(client);
+        }
+      });
+    });
+  }
 };
 
-MongoDB.prototype.getCollection = function(COLLECTION_NAME) {
-    if(!this.client) {
-        throw new Error('You MUST call getClient first');
-    }
-    const collection = this.client.db(this.DB_NAME).collection(COLLECTION_NAME);
-    console.log('success getCollection', collection.s.name);
-    return collection;
+MongoDB.prototype.getCollection = function (COLLECTION_NAME) {
+  if (!this.client) {
+    throw new Error('You MUST call getClient first');
+  }
+  const collection = this.client.db(this.DB_NAME).collection(COLLECTION_NAME);
+  console.log('success getCollection', collection.s.name);
+  return collection;
 };
 
-MongoDB.prototype.close = function() {
-    if(this.client) {
-        this.client.close();
+/**
+ * make filter
+ * @param options : [{field, operator, value}]
+ */
+MongoDB.prototype.makeFilter = function (options) {
+  let filter = {};
+  options.forEach(function (option) {
+    filter[option.field] = option.operator ? {} : option.value;
+    if (option.operator) {
+      filter[option.field][option.operator] = option.value;
     }
+  });
+  return filter;
+};
+
+MongoDB.prototype.close = function () {
+  if (this.client) {
+    this.client.close();
+  }
 };
 
 /**
@@ -48,41 +74,46 @@ MongoDB.prototype.close = function() {
  * @returns {Promise<any>}
  * @example : insert(collection, [{type: "pad"}, {type: "phone"}])
  */
-MongoDB.prototype.insert = function(collection, data) {
-    return new Promise((resolve, reject) => {
-        collection.insertMany(data, function (err, result) {
-            if (err) {
-                console.log('err insert', err);
-                reject(err);
-            } else {
-                console.log("insertMany the following records");
-                console.log(result);
-                resolve(result);
-            }
-        });
-    });
+MongoDB.prototype.insert = function (collection, data) {
+  return new Promise((resolve, reject) => {
+    const callback = function (err, result) {
+      if (err) {
+        console.log('err insert', err);
+        reject(err);
+      } else {
+        console.log("insertMany the following records");
+        console.log(result);
+        resolve(result);
+      }
+    };
+    if (Array.isArray(data)) {
+      collection.insertMany(data, callback);
+    } else {
+      collection.insertOne(data, callback);
+    }
+  });
 };
 
 /**
  * find data
  * @param collection
- * @param data
+ * @param filter
  * @returns {Promise<any>}
  * @exampel : find(collection, {type: "computer"})
  */
-MongoDB.prototype.find = function(collection, data) {
-    return new Promise((resolve, reject) => {
-        collection.find(data).toArray(function (err, docs) {
-            if (err) {
-                console.log('err find', err);
-                reject(err);
-            } else {
-                console.log("Found the following records");
-                console.log(docs);
-                resolve(docs);
-            }
-        });
+MongoDB.prototype.find = function (collection, filter) {
+  return new Promise((resolve, reject) => {
+    collection.find(filter).toArray(function (err, docs) {
+      if (err) {
+        console.log('err find', err);
+        reject(err);
+      } else {
+        console.log("Found the following records");
+        console.log(docs);
+        resolve(docs);
+      }
     });
+  });
 };
 
 module.exports = MongoDB;
--- test/indexTest.js
+++ test/indexTest.js
@@ -7,19 +7,35 @@ const DB_NAME = "test";
 const mongo = new MongoDB(MONGODB_PATH, DB_NAME);
 
 describe('test mongodb', function () {
-    //with --exit mocha options
-    it('#test connection', async function () {
-        this.timeout(10000);
-        await mongo.getClient();
-    });
+  //with --exit mocha options
+  it('#test connection', async function () {
+    this.timeout(10000);
+    await mongo.getClient();
+  });
 
-    it('#test insert', async function () {
-        const collection = await mongo.getCollection('devices');
-        await mongo.insert(collection, [{type: "tv"}, {type: "tablet"}]);
-    });
+  it('#test insert', async function () {
+    const collection = await mongo.getCollection('devices');
+    await mongo.insert(collection, [{type: "tv", price: 8873}, {
+      type: "tablet",
+      price: 8581,
+      secondHand: {owner: "test"}
+    }]);
+  });
 
-    it('#test find', async function () {
-        const collection = await mongo.getCollection('devices');
-        const result = await mongo.find(collection, {type: "tablet"});
-    });
+  it('#test find', async function () {
+    const collection = await mongo.getCollection('devices');
+    const options = [
+      {
+        field: 'type',
+        value: 'tablet'
+      },
+      {
+        field: 'price',
+        operator: mongo.COMPARISON_OPERATORS.greaterThan,
+        value: 50000
+      }
+    ];
+    const filter = await mongo.makeFilter(options);
+    const result = await mongo.find(collection, filter);
+  });
 });
